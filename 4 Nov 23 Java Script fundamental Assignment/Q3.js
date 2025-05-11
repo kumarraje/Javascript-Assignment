71b364c7903a72b9d426d627065d0a67b97e3ee5
Kumar Bhosale <70483720+kumarraje@users.noreply.github.com>
2025-05-11 15:50:24 +0530

    Add files via upload

---
+++ 4 Nov 23 Java Script fundamental Assignment/Q3.js
@@ -0,0 +1,41 @@
+//Unary Operators
+
+
+let x = 5;
+
+// Unary plus (+) – converts to number
+console.log(+true);       // Output: 1
+
+// Unary negation (-) – negates the value
+console.log(-x);          // Output: -5
+
+// Increment (++)
+x++;
+console.log(x);           // Output: 6
+
+// Decrement (--)
+x--;
+console.log(x);           // Output: 5
+
+// Logical NOT (!)
+console.log(!false);      // Output: true
+
+// typeof – returns the data type
+console.log(typeof x);    // Output: "number"
+
+
+//Binary Operators
+
+let a = 10, b = 5;
+
+// Arithmetic
+console.log(a + b);       // Output: 15
+
+// Assignment
+a = b;                    // a is now 5
+
+// Comparison
+console.log(a > b);       // Output: false
+
+// Logical
+console.log(true && false); // Output: false
