97c93005ed6886d701b99da27efa71f4692a4157
Kumar Bhosale <70483720+kumarraje@users.noreply.github.com>
2025-05-11 19:06:27 +0530

    Add files via upload

---
+++ 5 Nov 23 Java Script fundamental Assignment/Q4.js
@@ -0,0 +1,5 @@
+let name = "John";  // Declare a variable 'name' and assign it a string value
+let age = 30;       // Declare a variable 'age' and assign it a numeric value
+
+console.log(name);  // Output: John
+console.log(age);   // Output: 30
